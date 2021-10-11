785daebe1fe97e32256a38f5a877d3f6b7f25fe7
kawsarm104 <kawsarm104@gmail.com>
2021-10-11 16:22:19 +0600

    changed firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyA9QKxeRCIkJqCXnJMwzetm5Ou9NJupWv8",
-    authDomain: "ema-john-simple.firebaseapp.com",
-    databaseURL: "https://ema-john-simple.firebaseio.com",
-    projectId: "ema-john-simple",
-    storageBucket: "ema-john-simple.appspot.com",
-    messagingSenderId: "753249389312",
-    appId: "1:753249389312:web:2539987630fc0a25b201e5"
+    apiKey: "AIzaSyAMb5DrzsYjoC8I07aw_p_H7VCkCidVHto",
+  authDomain: "ema-john-with-authentica-a9542.firebaseapp.com",
+  projectId: "ema-john-with-authentica-a9542",
+  storageBucket: "ema-john-with-authentica-a9542.appspot.com",
+  messagingSenderId: "167904903742",
+  appId: "1:167904903742:web:1388b8056bf67b5a02876f"
 };
 
 export default firebaseConfig;
